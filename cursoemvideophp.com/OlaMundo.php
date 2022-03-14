dd09a2ff1c4f83befe837cbf0ab3a0d4a55894f1
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-03-14 20:33:35 -0300

    Ola Mundo em PHP

---
+++ cursoemvideophp.com/OlaMundo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Primeiro exemplo PHP</title>
+        <style>
+            h2 {
+                color: #00cafe;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+            echo "<h2>Olá Mundo!</h2>";
+        ?>
+    </body>
+</html>
