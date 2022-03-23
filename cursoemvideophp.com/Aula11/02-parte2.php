c1c6eabf067a6e7cf70ad06996913d3d70e7326d
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-03-23 16:24:26 -0300

    Estrutura de repetição while

---
+++ cursoemvideophp.com/Aula11/02-parte2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $i= 1;
+        while ($i <= 5) {
+            $v = "num".$i;
+            $i++;
+        }
+
+    ?>
+</div>
+</body>
+</html>
