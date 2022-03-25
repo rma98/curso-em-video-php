cac7674b96abbb6d949fafee5c2b87af18ad0348
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-03-25 17:55:45 -0300

    Vetores e matrizes parte 1

---
+++ cursoemvideophp.com/Aula18/01-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <pre>
+    <?php
+        $n = array(3,5,8,2);
+        $n[] = 7;
+        print_r($n);
+        echo "<hr>";
+        $c = range(5,20,2);
+        print_r($c);
+    ?>
+    </pre>
+</div>
+</body>
+</html>
+
---
+++ cursoemvideophp.com/Aula18/04-matriz.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <pre>
+    <?php
+        $m = array(
+                array(6,4),
+                array(4,9),
+                array(3,2),
+        );
+        print_r($m);
+    ?>
+    </pre>
+</div>
+</body>
+</html>
+
